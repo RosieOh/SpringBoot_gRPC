35cbc0a9fd27de14b38c6a07108724976b161aa1
RosieOh <dhxogns920@gmail.com>
2025-05-06 00:14:45 +0900

    FEAT : 엔티티 스캔 어노테이션 추가

--- src/main/java/com/netty/NettyApplication.java
+++ src/main/java/com/netty/NettyApplication.java
@@ -2,8 +2,10 @@ package com.netty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.netty.domain.entity")
 public class NettyApplication {
 
     public static void main(String[] args) {
