0631d3e33491e4716dde1b54fd89201ca9a5e13b
RosieOh <dhxogns920@gmail.com>
2025-05-06 00:15:47 +0900

    FEAT : 웹소켓 설젙 시 해시맵에 던지는게 아닌 자동 매핑으로 소켓 서버에 업로드 하는 방식으로 변경

--- src/main/java/com/netty/global/config/WebSocketConfig.java
+++ src/main/java/com/netty/global/config/WebSocketConfig.java
@@ -5,28 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class WebSocketConfig {
-    @Bean
-    public HandlerMapping webSocketMapping(NotificationWebSocketHandler handler) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("/ws/notifications", handler);
-
-        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
-        mapping.setUrlMap(map);
-        mapping.setOrder(10); // 우선순위
-
-        return mapping;
-    }
 
     @Bean
-    public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
+    public HandlerMapping handlerMapping(NotificationWebSocketHandler webSocketHandler) {
+        return new SimpleUrlHandlerMapping(Map.of(
+                "/ws/notifications", webSocketHandler
+        ), -1); // order 우선순위
     }
 }
-
