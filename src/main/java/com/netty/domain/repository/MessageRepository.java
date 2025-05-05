efaa73d731d7b37c4ff45d7c766509b4c1de13ac
RosieOh <dhxogns920@gmail.com>
2025-05-06 00:23:16 +0900

    FEAT : 알림 메시지 리액티브 비즈니스 레이어 추가

--- src/main/java/com/netty/domain/repository/MessageRepository.java
+++ src/main/java/com/netty/domain/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.netty.domain.repository;
 
 import com.netty.domain.entity.MessageEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface MessageRepository extends ReactiveCrudRepository<MessageEntity, Long> {
+    Mono<MessageEntity> findByUsername(String username);
 }
